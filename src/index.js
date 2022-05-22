9bd312889116ad92a7838e457f2b4e3236387ffc
szebestik@gmail.com <szebestik@gmail.com>
2022-05-22 17:37:49 +0200

    Revert test

--- src/index.js
+++ src/index.js
@@ -1,31 +1,30 @@
-// import { PrismaClient } from '@prisma/client'
-// import express from 'express'
-// const app = express()
-// const PORT = process.env.PORT ?? 3000
+import { PrismaClient } from '@prisma/client'
+import express from 'express'
+const app = express()
+const PORT = process.env.PORT ?? 3000
 
-// const prisma = new PrismaClient()
+const prisma = new PrismaClient()
 
-// app.get('/', async (req, res) => {
-//     try {
-//         const allUsers = await prisma.users.findMany({
-//             include: {
-//                 servers: true
-//             }
-//         })
+app.get('/', async (req, res) => {
+    try {
+        const allUsers = await prisma.users.findMany({
+            include: {
+                servers: true
+            }
+        })
 
-//         return res.json(allUsers).status(200)
-//     }
-//     catch(err) {
-//         console.log(err)
-//         res.sendStatus(500)
-//     }
-// })
+        return res.json(allUsers).status(200)
+    }
+    catch(err) {
+        console.log(err)
+        res.sendStatus(500)
+    }
+})
 
-// app.get('/login', async (req, res) => {
+app.get('/login', async (req, res) => {
 
-// })
+})
 
-// app.listen(PORT, () => {
-//   console.log(`Example app listening on port ${PORT}`)
-// })
-console.log('a')
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
