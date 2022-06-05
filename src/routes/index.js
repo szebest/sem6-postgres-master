7ba1a78ce13ab4788e930d6434660f90d502b990
szebestik@gmail.com <szebestik@gmail.com>
2022-06-05 19:12:08 +0200

    added slaves routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
 const userRoutes = require('./user/user')
+const slaveRoutes = require('./slaves/slaves')
 
 
 
 router = express.Router();
 
 router.use('/users', userRoutes)
+router.use('/slaves', slaveRoutes)
 
 module.exports = router;
